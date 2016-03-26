6b6c0355cd77d1b7e8929b18e83f4687985fd6c9
Brooke McFarland <brookelyn.mcf@gmail.com>
2016-03-26 16:25:33 -0500

    Started grayscale conversion function(working)

--- Program#5.py
+++ Program#5.py
@@ -1,7 +1,7 @@
 def get_user_file(user_file, readwrite):
     """get the name of the file to be edited from the user. Error check and print a prompt if an error is thrown."""
     while True:
-        file_given = input(user_file)#getting the input the user has entered
+        file_given = input(user_file,)#getting the input the user has entered
         try:
             correct_File = open(file_given, readwrite)
         except FileNotFoundError:
@@ -15,14 +15,18 @@ def get_filter(question, acceptable_answers):
     """Get input from user, decide what filter they would like to use"""
     while True:
         choice = input(question)
-        if choice=="1":
-            print("It works!!")
-        elif choice == "2":
-            print("woah buddy!")
-        elif choice == "3":
-            print("kick ass!")
-        else:
-            print("incorrect input, please choose from 1, 2, or 3.")
+        if choice in acceptable_answers:
+            return choice
+
+def grayscale(user_file):
+    lst = user_file.readlines()
+    if "P3\n" in lst:
+        print("P3")
+    elif "P3\n" not in lst:
+        print("The file you have submitted is not the correct file type. Please try another file.")
+
+
+
 
 
 
@@ -30,3 +34,13 @@ def get_filter(question, acceptable_answers):
 user_file = get_user_file("What is that name of the picture file you would like to edit?","r")
 file_created = get_user_file("Please name a file you would like to write your image too.","w")
 user_filter = get_filter("choose one of the following; 1.)Grayscale 2.)Vintage 3.)Quit",["1","2","3"]).lower()
+
+if user_filter=="1":
+    grayscale(user_file)
+    print("It works!!")
+elif user_filter == "2":
+    print("woah buddy!")
+elif user_filter == "3":
+    print("kick ass!")
+else:
+    print("incorrect input, please choose from 1, 2, or 3.")
